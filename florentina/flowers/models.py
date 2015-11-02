e552ab6011d07302907575e28a5897806c6d57ea
Maksadbek <a.maksadbek@gmail.com>
2015-11-02 23:26:36 +0500

    create flowers model fields

--- florentina/flowers/models.py
+++ florentina/flowers/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Flower(models.Model):
+    name = models.CharField(max_length=200)
+    desc = models.TextField()
+    info = models.TextField()
+    img = models.TextField()
