7af0e1642ff8fa5e520acc26fe6c269048496316
Maksadbek <a.maksadbek@gmail.com>
2015-11-06 01:24:07 +0500

    detail view for flower

--- florentina/flowers/urls.py
+++ florentina/flowers/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
         url(r'^$', views.index, name='index'),
+        url(r'^(?P<id>[0-9]+)/$', views.detail, name='detail'),
+
 ]
--- florentina/flowers/views.py
+++ florentina/flowers/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Flower
@@ -7,3 +7,8 @@ def index(request):
     flowers = Flower.objects.all()
     context = {'flowers':flowers}
     return render(request, 'flowers/index.html', context)
+
+def detail(request, id):
+    flower = get_object_or_404(Flower, id=id)
+    context = {'flower':flower}
+    return render(request, 'flowers/detail.html', context)
