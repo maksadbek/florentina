f2c07b86705a34bdfe50338ae07cb0d4535a3ded
Maksadbek <a.maksadbek@gmail.com>
2015-11-09 00:46:29 +0500

    order by category and sort by popularity or created

--- florentina/flowers/views.py
+++ florentina/flowers/views.py
@@ -2,13 +2,33 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Flower
+from .models import Category
 
 def index(request):
+    category_name = request.GET.get('category','')
+    sorting = request.GET.get('sorting', '')
     flowers = Flower.objects.order_by('category')
-    context = {'flowers':flowers}
-    return render(request, 'flowers/index.html', context)
+    # if category is not given, then show all
+    if category_name == "":
+        context = {'flowers':flowers}
+        return render(request, 'flowers/index.html', context)
+    # if category is given, filter by category and sort by popularity by default
+    if sorting not in ["popularity", "created"]:
+        sorting = "popularity"
+
+    context = { 
+        'sorting': sorting, 
+        'category':category_name,
+        'flowers':flowers.filter(category__name=category_name).order_by("-"+sorting) ,
+    }
+    return render(request, 'flowers/catalog.html', context)
 
 def detail(request, id):
     flower = get_object_or_404(Flower, id=id)
+    context = {'flower':flower}
+    return render(request, 'flowers/detail.html', context)
+
+    category_name = request.GET.get('category','')
+    flowers = Flower.objects.filter(category__name=category)
     context = {'flower':flower}
     return render(request, 'flowers/detail.html', context)
