5bb7b95f5111e69cd0808a73c96549263d7d033a
shstem <sh.stem@gmail.com>
2015-12-02 03:11:11 +0500

    added modals for signin and signup

--- florentina/static/js/script.js
+++ florentina/static/js/script.js
@@ -1,1 +1,13 @@
 $("input[type=number]").numberfield();
+
+var signUpBtn = ".signup",
+	signInBtn = ".signin";
+
+$(signUpBtn+","+signInBtn).on('click', function ( e ) {
+    var link = $(this).attr("href");
+    Custombox.open({
+        target: link,
+        effect: 'blur'
+    });
+    e.preventDefault();
+});
