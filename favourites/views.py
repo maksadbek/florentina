cde4721a1220762a6c29c758e1c1b0657907d4b0
= <=>
2015-12-17 00:00:18 +0500

    favourites add/delete controllers are responding with statuses

--- favourites/views.py
+++ favourites/views.py
@@ -1,27 +1,29 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from favourites.models import Favourite
 from flowers.models import Flower
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/')
 def show(request):
     print Favourite.objects.all()
     f = Favourite.objects.filter(user=request.user)
     context = {"favourites":f}
     return render(request, "favourites/show.html", context)
 
+@login_required(login_url='/')
 def new(request):
     postdata = request.POST.copy()
     flower_id = postdata.get("flower_id")
-    print flower_id
     flower = get_object_or_404(Flower, id=flower_id)
     favourite = Favourite() 
     favourite.user = request.user
     favourite.flower = flower
     favourite.save()
-    return HttpResponse("ok")
+    return HttpResponse(status=201)
 
+@login_required(login_url='/')
 def delete(request, id):
-    id = id
     favourite = get_object_or_404(Favourite, id=id)
     favourite.delete()
-    return HttpResponse("ok")
+    return HttpResponse(status=204)
